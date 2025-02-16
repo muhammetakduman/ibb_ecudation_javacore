9ee3a81cd3566bd9f36b146b714bef08a7db8a65
Muhammet Akduman <akdumanmxc@gmail.com>
2025-02-16 16:48:11 +0300

    Date calender

--- src/main/java/org/example/_2week/_15_1_Date.java
+++ src/main/java/org/example/_2week/_15_1_Date.java
@@ -20,7 +20,48 @@ public class _15_1_Date {
 
     };
 
+    public static String newFormat1() throws NullPointerException {
+        Date now = new Date();
+        String specialFormat = "Şimdiki zaman : "
+                .concat(String.valueOf(now.getHours()))
+                .concat(":")
+                .concat(String.valueOf(now.getMinutes()))
+                .concat(":")
+                .concat(String.valueOf(now.getSeconds()))
+                .toString();
+        return specialFormat;
+    }
+
+    //Second Method :
+    public static String newFormat2() throws NullPointerException {
+        Date now = new Date();
+        Locale locale = new Locale("tr","TR");
+        //%s :string , %d Decimal , %f : Float
+        return String.format("Şimdiki zaman : %02d:%02d:%02d ", now.getHours(),now.getMinutes(),now.getSeconds());
+    }
+
+        public static String nowFormat3() throws NullPointerException {
+            Date now = new Date();
+            Locale locale = new Locale("tr", "TR");
+
+            //SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss",locale);
+            //SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MMM-dd HH:mm:ss",locale);
+            //SimpleDateFormat sdf = new SimpleDateFormat("dd/MMMM/yyyy HH:mm:ss",locale);
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MMMM/yyyy hh:mm:ss", locale);
+            //SimpleDateFormat sdf = new SimpleDateFormat("dd/MMMM/yyyy HH:mm:ss zzzz",locale);
+
+            String formatedDate = String.format("Şimdiki Zaman: %s", sdf.format(now));
+            return new Date().toString() + " - " + formatedDate;
+    }
+
+
     public static void main(String[] args) {
-        dateGetMethod();
+        //dateGetMethod();
+        //String nowDate = newFormat1();
+        //System.out.println(newFormat1());
+        //newFormat2();
+        //System.out.println(newFormat2());
+        String nowDate3 = nowFormat3();
+        System.out.println(nowDate3);
     }
 }
