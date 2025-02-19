0a9c928af8ef2578986c823fd5db75034d3acdb7
Muhammet Akduman <akdumanmxc@gmail.com>
2025-02-20 01:05:15 +0300

    oop

---
+++ src/main/java/org/example/_2week/House.java
@@ -0,0 +1,13 @@
+package org.example._2week;
+
+public class House extends oopExHouse {
+    public static void main(String[] args) {
+        House house1 = new House();
+        House house2 = new House();
+        house1.glassShape = "Square";
+        System.out.println(house1.glassShape);
+        System.out.println(house2.doorColor);
+        System.out.println(house2.roofColor);
+        System.out.println(house1.wasBuildYear);
+    }
+}
