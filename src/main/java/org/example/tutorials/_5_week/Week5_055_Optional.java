ebb1073d1e897f0d8b883ac1bb1fc761d8afe510
Muhammet Akduman <akdumanmxc@gmail.com>
2025-03-13 02:52:07 +0300

    optional

--- src/main/java/org/example/tutorials/_5_week/Week5_055_Optional.java
+++ src/main/java/org/example/tutorials/_5_week/Week5_055_Optional.java
@@ -1,6 +1,25 @@
 package org.example.tutorials._5_week;
 
+import lombok.Getter;
+import lombok.Setter;
+
+
+
 
 public class Week5_055_Optional {
+    public String getName(String data) {
+        if (data.isEmpty() || data==null){
+            return data;
+        } else {
+            return "Unknow";
+        }
+    }
+
+    public static void main(String[] args) {
+        Week5_055_Optional week5055Optional = new Week5_055_Optional();
+        String result = week5055Optional.getName(" ");
+        System.out.println(result);
+
+    }
 
 }
